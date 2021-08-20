f16ab6b1e601f5445ef9016dd373be641388c295
Vess <33204181+vess-dev@users.noreply.github.com>
2021-08-20 15:44:15 -0400

    working on threading

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,117 @@
 use rand::distributions::{Distribution, Weighted, WeightedChoice};
 use std::env;
 use std::process::exit;
+use rand::SeedableRng;
+use std::thread;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn help() {
-    println!("usage:
+	println!("usage:
 
-drop chance: f64 = (0, 100)
-chests: u64 = (0, max]
-trials: u64 = (0, max]
+drop chance: f32 = (0.0, 100.0)
+chests: u32 = (0, max]
+trials: u32 = (0, max]
 
 drop <drop chance> <chests> <trials>
 	Check the total chance out of N trials to receive 1 item out of N chests if the item has N drop chance.
 
 Example usage:
-	drop 4.55 20 10000000");
+	drop 4.55 20 10,000,000");
+}
+
+// Collect the args and verify them.
+fn verify() -> (f32, u32, u32) {
+	let sys_args: Vec<String> = env::args().collect();
+	match sys_args.len() {
+		1 => {
+			help();
+			exit(1);
+		}
+		4 => {
+			let test_drop = sys_args[1].parse::<f32>();
+			let test_chests = sys_args[2].parse::<u32>();
+			let test_trials = sys_args[3].replace(",", "").parse::<u32>();
+			if let Err(_e) = test_drop {
+				println!("Error: drop chance: f32 = (0.0, 100.0)\n");
+				help();
+				exit(1);
+			} else if let Ok(value) = test_drop {
+				if value >= 100.0 {
+					println!("Error: drop chance >= 100.0\n");
+					help();
+					exit(1);
+				}
+			}
+			if let Err(_e) = test_chests {
+				println!("Error: chests: u32 = (0, max]\n");
+				help();
+				exit(1);
+			} else if let Ok(value) = test_chests {
+				if value == 0 {
+					println!("Error: chest = 0\n");
+					help();
+					exit(1);
+				}
+			}
+			if let Err(_e) = test_trials {
+				println!("Error: trials: u32 = (0, max]\n");
+				help();
+				exit(1);
+			} else if let Ok(value) = test_trials {
+				if value == 0 {
+					println!("Error: trials = 0\n");
+					help();
+					exit(1);
+				}
+			}
+			// Return if all are correct.
+			return (test_drop.unwrap(), test_chests.unwrap(), test_trials.unwrap());
+		}
+		_ => {
+			println!("Incorrect number of arguments.\n");
+			help();
+			exit(1);
+		}
+	}
+}
+
+// Simulate one drop of a chest.
+fn drop(arg_drop: u32, arg_other: u32) -> bool {
+	let mut drop_weight = vec![
+				Weighted {
+					weight: arg_drop,
+					item: true,
+				},
+				Weighted {
+					weight: arg_other,
+					item: false,
+				},
+			];
+	let sys_time = SystemTime::now().duration_since(UNIX_EPOCH).expect("Duration since UNIX_EPOCH failed.");
+	let mut sys_rng = rand::rngs::StdRng::seed_from_u64(sys_time.as_secs());
+	let chest_drop = WeightedChoice::new(&mut drop_weight);
+	return chest_drop.sample(&mut sys_rng);
 }
 
 fn main() {
-    // Catch incorrect arguments.
-    let sys_args: Vec<String> = env::args().collect();
-    match sys_args.len() {
-        1 => {
-            help();
-            exit(1);
-        }
-        4 => {
-            let test_drop = sys_args[1].parse::<f64>();
-            let test_chests = sys_args[2].parse::<u64>();
-            let test_trials = sys_args[3].parse::<u64>();
-            if let Err(_e) = test_drop {
-                println!("Error: drop chance: f64 = (0, 100)\n");
-                help();
-                exit(1);
-            } else if let Ok(value) = test_drop {
-                if value >= 100.0 {
-                    println!("Error: drop chance >= 100\n");
-                    help();
-                    exit(1);
-                }
-            }
-            if let Err(_e) = test_chests {
-                println!("Error: chests: u64 = (0, max]\n");
-                help();
-                exit(1);
-            } else if let Ok(value) = test_chests {
-                if value == 0 {
-                    println!("Error: chest = 0\n");
-                    help();
-                    exit(1);
-                }
-            }
-            if let Err(_e) = test_trials {
-                println!("Error: trials: u64 = (0, max]\n");
-                help();
-                exit(1);
-            } else if let Ok(value) = test_trials {
-                if value == 0 {
-                    println!("Error: trials = 0\n");
-                    help();
-                    exit(1);
-                }
-            }
-        }
-        _ => {
-            println!("Incorrect number of arguments.\n");
-            help();
-            exit(1);
-        }
-    }
-    // Create a weighted choice.
-    let weight_drop = (sys_args[1].parse::<f64>().unwrap() * 100.0) as u32;
-    let weight_other = 10_000 - weight_drop;
-    let mut drop_weight = vec![
-        Weighted {
-            weight: weight_drop,
-            item: true,
-        },
-        Weighted {
-            weight: weight_other,
-            item: false,
-        },
-    ];
-    let drop_chest = WeightedChoice::new(&mut drop_weight);
-    let mut sys_rng = rand::thread_rng();
-    // Total trials where 1 item was pulled.
-    let mut trials_success = 0;
-    let trials_chests = sys_args[2].parse::<u64>().unwrap();
-    let trials_total = sys_args[3].parse::<u64>().unwrap();
-    for _trial in 0..trials_total {
-        for _chest in 0..trials_chests {
-            if drop_chest.sample(&mut sys_rng) {
-                trials_success += 1;
-                break;
-            }
-        }
-    }
-    let trials_perc: f64 = (100.0 / trials_total as f64) * trials_success as f64;
-    println!("Out of [{}] trials an item with a drop chance of [{}%] was dropped from [{}] chests [{:.2}%] of the time.", trials_total, weight_drop as f64 / 100.0, trials_chests, trials_perc);
+	let (arg_drop, arg_chests, arg_trials) = verify();
+	// Create a weighted choice.
+	let weight_drop = (arg_drop * 100.0) as u32;
+	let weight_other = 10_000 - weight_drop;
+	// Create a thread per trial.
+	let mut trials_success: u64 = 0;
+	let mut trials_threads = Vec::new();
+	for _trial in 0..arg_trials {
+		let trial_thread = thread::spawn(move || {
+			for _chest in 0..arg_chests {
+				continue;
+			}
+		});
+		trials_threads.push(trial_thread);
+	}
+	for temp_thread in trials_threads {
+		temp_thread.join().expect("Thread panicked.");
+	}
+	let trials_perc: f64 = (100.0 / arg_trials as f64) * trials_success as f64;
+	println!("Out of [{}] trials an item with a drop chance of [{}%] was dropped from [{}] chests [{:.2}%] of the time.", arg_trials, weight_drop as f64 / 100.0, arg_chests, trials_perc);
 }
