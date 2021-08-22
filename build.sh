b9841443650f674a7d9ee8d664ec1dee3a714870
Vess <vess.contact@protonmail.com>
2021-08-22 07:30:37 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# Build Linux.
+# Build for Linux.
 cargo build --release
 strip target/release/drop
 sstrip target/release/drop
 upx --lzma target/release/drop
-# Crosscompile Windows.
-# You will need to execute these manually.
-# cargo build --release --target x86_64-pc-windows-gnu
-# strip target/x86_64-pc-windows-gnu/release/drop.exe
-# upx --lzma target/x86_64-pc-windows-gnu/release/drop.exe
